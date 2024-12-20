bafc772281168430ce8d1375d8e0dee4ba750aa3
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2024-12-20 21:53:03 +0100

    Convert indentation to tabs

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ static inline int touch_callback(int device, struct finger *fingers, int nFinger
 }
 
 static CGEventRef mouse_callback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
-    static bool is_middle_click = false;
+	static bool is_middle_click = false;
 
 	if (current_fingers != 3 && !is_middle_click) {
 		return event;
@@ -87,62 +87,62 @@ static inline void devices_cleanup(struct mt_devices devices) {
 }
 
 static inline void devices_refresh(struct mt_devices *devices) {
-    devices_cleanup(*devices);
-    *devices = multitouch_devices();
-    devices_register(*devices, touch_callback);
+	devices_cleanup(*devices);
+	*devices = multitouch_devices();
+	devices_register(*devices, touch_callback);
 }
 
 static void device_notification_callback(void *refcon, io_iterator_t iter) {
-    devices_refresh((struct mt_devices *) refcon);
+	devices_refresh((struct mt_devices *) refcon);
 }
 
 static inline kern_return_t listen_io_notification(struct mt_devices *devices, IONotificationPortRef port) {
 	// Set up device notifications
-    CFRunLoopAddSource(
-        CFRunLoopGetMain(),
-        IONotificationPortGetRunLoopSource(port),
-        kCFRunLoopDefaultMode
-    );
+	CFRunLoopAddSource(
+		CFRunLoopGetMain(),
+		IONotificationPortGetRunLoopSource(port),
+		kCFRunLoopDefaultMode
+	);
 
-    io_iterator_t iterator;
-    kern_return_t kres = IOServiceAddMatchingNotification(
-        port,
-        kIOFirstMatchNotification,
-        IOServiceMatching("AppleMultitouchDevice"),
-        device_notification_callback,
-        devices,
-        &iterator
-    );
+	io_iterator_t iterator;
+	kern_return_t kres = IOServiceAddMatchingNotification(
+		port,
+		kIOFirstMatchNotification,
+		IOServiceMatching("AppleMultitouchDevice"),
+		device_notification_callback,
+		devices,
+		&iterator
+	);
 
-    if (kres != KERN_SUCCESS) {
-        IONotificationPortDestroy(port);
-        return kres;
-    }
+	if (kres != KERN_SUCCESS) {
+		IONotificationPortDestroy(port);
+		return kres;
+	}
 
-    io_object_t item;
-    while ((item = IOIteratorNext(iterator))) {
-        IOObjectRelease(item);
-    }
-    return kres;
+	io_object_t item;
+	while ((item = IOIteratorNext(iterator))) {
+		IOObjectRelease(item);
+	}
+	return kres;
 }
 
 static int listen_click_loop(struct mt_devices *devices) {
-    CFMachPortRef tap_event = NULL;
+	CFMachPortRef tap_event = NULL;
 
-    for (int i = 0; tap_event == NULL && i < 300; i++) {
-        // Create a global event tap to listen for left mouse down and up events
-    	tap_event = CGEventTapCreate(
-    		kCGHIDEventTap,
-    		kCGHeadInsertEventTap,
-    		kCGEventTapOptionDefault,
-    		(1 << kCGEventLeftMouseDown) | (1 << kCGEventLeftMouseUp),
-    		mouse_callback,
-    		NULL
-    	);
-        if (tap_event == NULL) {
-            sleep(1);
-        }
-    }
+	for (int i = 0; tap_event == NULL && i < 300; i++) {
+		// Create a global event tap to listen for left mouse down and up events
+		tap_event = CGEventTapCreate(
+			kCGHIDEventTap,
+			kCGHeadInsertEventTap,
+			kCGEventTapOptionDefault,
+			(1 << kCGEventLeftMouseDown) | (1 << kCGEventLeftMouseUp),
+			mouse_callback,
+			NULL
+		);
+		if (tap_event == NULL) {
+			sleep(1);
+		}
+	}
 
 	if (tap_event == NULL) {
 		fputs("Failed to create event tap. Check accessibility permissions.", stderr);
@@ -165,28 +165,28 @@ static int listen_click_loop(struct mt_devices *devices) {
 
 	// If for some reason the main loop returns we cleanup the registered events.
 	CGEventTapEnable(tap_event, false);
-   	CFRunLoopRemoveSource(CFRunLoopGetCurrent(), run_loop_src, kCFRunLoopCommonModes);
-   	CFRelease(run_loop_src);
-   	CFRelease(tap_event);
+	CFRunLoopRemoveSource(CFRunLoopGetCurrent(), run_loop_src, kCFRunLoopCommonModes);
+	CFRelease(run_loop_src);
+	CFRelease(tap_event);
 	return 0;
 }
 
 int main() {
-    struct mt_devices devices = multitouch_devices();
-    devices_register(devices, touch_callback);
+	struct mt_devices devices = multitouch_devices();
+	devices_register(devices, touch_callback);
 
-    IONotificationPortRef port = IONotificationPortCreate(kIOMainPortDefault);
+	IONotificationPortRef port = IONotificationPortCreate(kIOMainPortDefault);
 	if (listen_io_notification(&devices, port) != KERN_SUCCESS) {
-        fputs("Failed to add device notification.", stderr);
-        devices_cleanup(devices);
-        return 1;
-    }
+		fputs("Failed to add device notification.", stderr);
+		devices_cleanup(devices);
+		return 1;
+	}
 
 	for (;;) {
-	   if (listen_click_loop(&devices) != 0) {
+		if (listen_click_loop(&devices) != 0) {
 			devices_cleanup(devices);
 			IONotificationPortDestroy(port);
-            return 1;
+			return 1;
 		}
 	}
 	return 0;
